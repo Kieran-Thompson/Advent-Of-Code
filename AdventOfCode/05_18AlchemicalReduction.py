41c007d46442487c5271b1b937ab22993f8dcd43
Kieran Thompson <35673859+Kieran-Thompson@users.noreply.github.com>
2018-12-06 19:06:18 +0000

    Add files via upload

--- AdventOfCode/05_18AlchemicalReduction.py
+++ AdventOfCode/05_18AlchemicalReduction.py
@@ -18,30 +18,49 @@ def reacting(a,A):
             return False
     return False
 
-##main run
-file = open("input.txt","r")
-l = file.readline()
-ans = []
-ans.append(l[0])
-for i in l[1:]:
-    if len(ans) == 0:
-        ans.append(i)
-    else:
-        if reacting(ans[-1],i) == True:
-            ans.pop()
-        
-            
-        
-        else:
+##main run    
+
+def polymers():
+    file = open("input.txt","r")
+    l = file.readline()
+    ans = []
+    ans.append(l[0])
+    for i in l[1:]:
+        if len(ans) == 0:
             ans.append(i)
-        
-        
-print(len(ans))
-    
-    
+        else:
+            if reacting(ans[-1],i) == True:
+                ans.pop()                    
+            else:
+                ans.append(i)
+    return len(ans)
 
+##part 2
 
+##get the polymer that does the most reduction, when it has been removed
 
+###get the best polymer
+def bestpolymer():
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    values = []
+    file = open("input.txt","r")
+    l = file.readline()
+    for letter in alphabet:
+        a =l.replace(letter,"")
+        a = a.replace(letter.upper(),"")
+        ans = []
+        ans.append(a[0])
+        for i in a[1:]:
+            if len(ans) == 0:
+                ans.append(i)
+            else:
+                if reacting(ans[-1],i) == True:
+                    ans.pop()                    
+                else:
+                    ans.append(i)
+        values.append(len(ans))
+    return min(values)
+print(bestpolymer())
 
 
 
