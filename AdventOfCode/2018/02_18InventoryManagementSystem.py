39d4f2e84ac6130411ec131594f242dbf7cb3441
Kieran Thompson <35673859+Kieran-Thompson@users.noreply.github.com>
2020-02-27 22:31:00 +0000

    part 2 of 02_18

--- AdventOfCode/2018/02_18InventoryManagementSystem.py
+++ AdventOfCode/2018/02_18InventoryManagementSystem.py
@@ -13,8 +13,6 @@ def readfile():
 
 ##Part 1
 
-
-
 ##given input, return the number of element repeated 2 and 3 times, multipled together
 def checkSum():
     
@@ -36,7 +34,30 @@ def checkSum():
         if 3 in dico.values():
             three = three + 1
             
-        
-        
     return three*two
+
+##Part 2
+def loopThroughlist(listOfStrings):
+    for i in range(len(listOfStrings)-1):
+        hamming_set(listOfStrings, listOfStrings[i])
+
+
+def hamming_set(listOfStrings, string1):
+    for item in listOfStrings:
+        if (1 == hamming_distance(item, string1)):
+            print(item, string1)
+        
+
+def hamming_distance(string1, string2): 
+    distance = 0
+    for i in range(len(string1)-1):
+        if string1[i] != string2[i]:
+            distance += 1
+    return distance
+
+file = open("input.txt", "r")
+readlist = file.readlines()
+loopThroughlist(readlist)
+
+##Strings easy to compare
 
